d9da5a4e874e240ceabf1023d7395b0b3a53f628
OswaldBarbosa <oswaldo.barbosacruz1902@gmail.com>
2023-03-23 11:31:06 -0300

    projeto quase completo

--- ds2t/oswaldo_da_silva_barbosa_dos_santos/JS/index.js
+++ ds2t/oswaldo_da_silva_barbosa_dos_santos/JS/index.js
@@ -40,17 +40,7 @@ const criarHeaderMensagens = (indice) => {
     nomeContato.classList.add('title__mensagens')
     nomeContato.textContent = contatos[indice].name
 
-    const searchContainer = document.createElement('div')
-    searchContainer.classList.add('search__mensagens')
-
-    const iconSearch = document.createElement('i')
-    iconSearch.class = 'fas fa-search'
-
-    const iconSettings = document.createElement('i')
-    iconSearch.classList.add('settings__mensagens')
-
-    header.append(foto, nomeContato, searchContainer)
-    searchContainer.append(iconSearch)
+    header.append(foto, nomeContato)
 
     return header
 }
@@ -59,25 +49,67 @@ const criarContainerMensagens = (indice) => {
     const containerMensagens = document.createElement('div')
     containerMensagens.classList.add('container__mensagens')
 
+    contatos[indice].messages.forEach((mensagens) => {
+
+        const caixaMensagemMinha = document.createElement('div')
+        caixaMensagemMinha.classList.add('caixa-mensagem-minha')
+
+        const mensagemMinha = document.createElement('p')
+        mensagemMinha.classList.add('mensagem-minha')
+
+        const horaMinha = document.createElement('p')
+        horaMinha.classList.add('hora-minha')
+
+        const caixaMensagemSua = document.createElement('div')
+        caixaMensagemSua.classList.add('caixa-mensagem-sua')
+
+        const mensagemSua = document.createElement('p')
+        mensagemSua.classList.add('mensagem-sua')
+
+        const horaSua = document.createElement('p')
+        horaSua.classList.add('hora-sua')
+
+        if (mensagens.sender == "me") {
+            mensagemMinha.classList.add('mensagem-minha')
+            mensagemMinha.textContent = mensagens.content
+
+            horaMinha.classList.add('hora-minha')
+            horaMinha.textContent = mensagens.time
+
+            containerMensagens.append(caixaMensagemMinha, caixaMensagemSua)
+            caixaMensagemSua.append(mensagemSua, horaSua)
+
+        } else if (mensagens.sender == contatos[indice].name) {
+            mensagemSua.classList.add('mensagem-sua')
+            mensagemSua.textContent = mensagens.content
+
+            horaSua.classList.add('hora-sua')
+            horaSua.textContent = mensagens.time
+
+            containerMensagens.append(caixaMensagemMinha, caixaMensagemSua)
+            caixaMensagemSua.append(mensagemSua, horaSua)
+        }
+    })
     return containerMensagens
 }
 
-const criarFooterMensagens = (indice) => {
+const criarFooterMensagens = () => {
     const containerMensagensFooter = document.createElement('div')
     containerMensagensFooter.classList.add('footer__mensagens')
 
-    const containerIconsFooter = document.createElement('div')
-    containerIconsFooter.classList.add('emoji__anexar')
+    const icons = document.getElementById('emoji__anexar')
+    icons.classList.remove('emoji__anexar')
+    icons.classList.add('emoji__anexar__on')
 
-    const icon = document.createElement('i')
-    icon.classList.add('emoji__anexar')
+    const iconMicrofone = document.getElementById('iconMicrofone')
+    iconMicrofone.classList.remove('microfone-none')
+    iconMicrofone.classList.add('microfone')
 
     const inputMensagem = document.createElement('input')
+    inputMensagem.placeholder = 'Mensagem'
     inputMensagem.classList.add('input__mensagem')
 
-    containerMensagensFooter.append(inputMensagem, containerIconsFooter)
-
-    containerIconsFooter.append(icon)
+    containerMensagensFooter.append(inputMensagem, icons, iconMicrofone)
 
     return containerMensagensFooter
 }
